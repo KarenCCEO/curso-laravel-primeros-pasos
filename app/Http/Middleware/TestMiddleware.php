b68ba60d4e2d4205c13ffb270a8e4b21eab53b60
libredesarrollo <desarrollolibre.net@gmail.com>
2022-02-28 11:33:09 -0400

    varias

--- app/Http/Middleware/TestMiddleware.php
+++ app/Http/Middleware/TestMiddleware.php
@@ -7,17 +7,9 @@ use Illuminate\Http\Request;
 
 class TestMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
     public function handle(Request $request, Closure $next)
     {
-      //  return redirect("/");
-
+        // return redirect("/");
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,50 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([App\Http\Middleware\TestMiddleware::class])->group(function () {
-    Route::get('/test/{id?}/{name?}', function ($id = 10, $name = "pepe") {
-        echo $id;
-        echo $name;
-    });
-});
+
 
 Route::group(['prefix' => 'dashboard'], function () {
-     Route::resource('post', PostController::class)->except(['show']);
-    // Route::resource('category', CategoryController::class);
-
-    // Route::resources([
-    //     'post' => PostController::class,
-    //     'category' => CategoryController::class,
-    // ]);
-
+    Route::resources([
+        'post' => PostController::class,
+        'category' => CategoryController::class,
+    ]);
 });
-
-
-// Route::get('/test/{id?}/{name?}', function ($id = 10, $name = "pepe") {
-//     echo $id;
-//     echo $name;
-// });
-// Route::controller(PostController::class)->group(function () {
-//     Route::get('post','index')->name("post.index");
-//     Route::get('post/{post}','show')->name("post.show");
-//     Route::get('post/create','create')->name("post.create");
-//     Route::get('post/{post}/edit','edit')->name("post.edit");
-
-//     Route::post('post','store')->name("post.store");
-//     Route::put('post/{post}','update')->name("post.update");
-//     Route::delete('post/{post}','delete')->name("post.destroy");
-// });
-
-
-
-
-
-
-// Route::get('post', [PostController::class, 'index']);
-// Route::get('post/{post}', [PostController::class, 'show']);
- //Route::get('post/create', [PostController::class, 'create']);
-// Route::get('post/{post}/edit', [PostController::class, 'edit']);
-
-// Route::post('post', [PostController::class, 'store']);
-// Route::put('post/{post}', [PostController::class, 'update']);
-// Route::delete('post/{post}', [PostController::class, 'delete']);
